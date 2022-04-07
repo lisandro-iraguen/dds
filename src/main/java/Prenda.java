037aed290490e1a65388bdfb4e4e38dfeca71971
lisandro <lisandroiraguen@chevron.com>
2022-04-07 08:49:52 -0300

    diagrama

--- src/main/java/Prenda.java
+++ src/main/java/Prenda.java
@@ -4,11 +4,12 @@ import Estado.IEstado;
 public class Prenda {
     public String tipo;
     public IEstado estado;
+    private float precio=0;
 
     public void setTipo(String nombre){
         tipo=nombre;
     }
     public float precio(){
-        return 0;
+        return precio;
     }
 }
